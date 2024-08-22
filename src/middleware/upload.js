733b86bc7573117e24767e0739679187549c8e81
khanakitclass <khanakitclass@gmail.com>
2024-08-22 18:12:09 +0530

    xc

--- src/middleware/upload.js
+++ src/middleware/upload.js
@@ -2,67 +2,42 @@ const multer = require('multer');
 const fs = require('fs');
 const path = require('path');
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        const folderName = file.fieldname;
-        const uploadPath = path.join('../public', folderName);
 
-        fs.mkdir(uploadPath, { recursive: true }, function (err) {
-            if (err) {
-                return cb(err);
-            }
-            cb(null, uploadPath);
-        });
-    },
-    filename: function (req, file, cb) {
-        cb(null, `${Date.now()}-${file.originalname}`);
-    }
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    const fieldname = file.fieldname;
+    // const pathUpload = path.join('public', fieldname);
+
+    // const pathUpload = path.resolve(__dirname, '../../public', fieldname);
+
+    const pathUpload = path.join("/tmp", fieldname);
+
+  //   const pathUpload = path.join(
+  //     process.cwd(),
+  //     fieldname
+  // );
+
+    console.log(pathUpload,fieldname);
+    
+
+    fs.mkdirSync(pathUpload, { recursive: true }, (err) => {
+      if (err) {
+        return cb(err, null);
+      }
+    });
+
+    cb(null, pathUpload);
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    cb(null, uniqueSuffix + '-' + file.originalname);
+  }
 });
 
 const upload = multer({ storage: storage });
 
 module.exports = upload;
-
-// const multer = require('multer');
-// const fs = require('fs');
-// const path = require('path');
-
-
-
-// const storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     const fieldname = file.fieldname;
-//     const pathUpload = path.join('public', fieldname);
-
-//     // const pathUpload = path.resolve(__dirname, '../../public', fieldname);
-
-//     // const pathUpload = path.join("/tmp", fieldname);
-
-//   //   const pathUpload = path.join(
-//   //     process.cwd(),
-//   //     fieldname
-//   // );
-
-//     console.log(pathUpload,fieldname);
-    
-
-//     fs.mkdirSync(pathUpload, { recursive: true }, (err) => {
-//       if (err) {
-//         return cb(err, null);
-//       }
-//     });
-
-//     cb(null, pathUpload);
-//   },
-//   filename: function (req, file, cb) {
-//     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-//     cb(null, uniqueSuffix + '-' + file.originalname);
-//   }
-// });
-
-// const upload = multer({ storage: storage });
-
-// module.exports = upload;
 
 // const multer = require('multer');
 // const fs = require('fs');
