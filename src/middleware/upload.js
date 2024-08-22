666127908bcd2ffd3fb70483eb55809493d2df1f
khanakitclass <khanakitclass@gmail.com>
2024-08-22 16:42:02 +0530

    dfv

--- src/middleware/upload.js
+++ src/middleware/upload.js
@@ -1,64 +1,30 @@
-// const multer = require('multer');
-// const fs = require('fs');
-// const path = require('path');
-
-
-
-// const storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     const fieldname = file.fieldname;
-//     // const pathUpload = path.resolve(__dirname, '../../public', fieldname);
-
-//     const pathUpload = path.join("/tmp", fieldname);
-
-//   //   const pathUpload = path.join(
-//   //     process.cwd(),
-//   //     fieldname
-//   // );
-
-//     console.log(pathUpload,fieldname);
-    
-
-//     fs.mkdirSync(pathUpload, { recursive: true }, (err) => {
-//       if (err) {
-//         return cb(err, null);
-//       }
-//       cb(null, pathUpload); 
-//     });
-//   },
-//   filename: function (req, file, cb) {
-//     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-//     cb(null, uniqueSuffix + '-' + file.originalname);
-//   }
-// });
-
-// const upload = multer({ storage: storage });
-
-// module.exports = upload;
-
 const multer = require('multer');
 const fs = require('fs');
 const path = require('path');
 
 
-const isVercel = process.env.VERCEL || process.env.NOW_REGION;
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     const fieldname = file.fieldname;
+    // const pathUpload = path.resolve(__dirname, '../../public', fieldname);
 
-  
-    const pathUpload = isVercel 
-      ? path.join('/tmp', fieldname)
-      : path.join('public', fieldname);
+    const pathUpload = path.join("/tmp", fieldname);
 
-   
-    console.log(`Uploading to: ${pathUpload}`);
+  //   const pathUpload = path.join(
+  //     process.cwd(),
+  //     fieldname
+  // );
 
-  
-    fs.mkdirSync(pathUpload, { recursive: true });
+    console.log(pathUpload,fieldname);
+    
 
-    cb(null, pathUpload); 
+    fs.mkdirSync(pathUpload, { recursive: true }, (err) => {
+      if (err) {
+        return cb(err, null);
+      }
+      cb(null, pathUpload); 
+    });
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
@@ -69,4 +35,38 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 
 module.exports = upload;
+
+// const multer = require('multer');
+// const fs = require('fs');
+// const path = require('path');
+
+
+// const isVercel = process.env.VERCEL || process.env.NOW_REGION;
+
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     const fieldname = file.fieldname;
+
+  
+//     const pathUpload = isVercel 
+//       ? path.join('/tmp', fieldname)
+//       : path.join('public', fieldname);
+
+   
+//     console.log(`Uploading to: ${pathUpload}`);
+
+  
+//     fs.mkdirSync(pathUpload, { recursive: true });
+
+//     cb(null, pathUpload); 
+//   },
+//   filename: function (req, file, cb) {
+//     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+//     cb(null, uniqueSuffix + '-' + file.originalname);
+//   }
+// });
+
+// const upload = multer({ storage: storage });
+
+// module.exports = upload;
 
